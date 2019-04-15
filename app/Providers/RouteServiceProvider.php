55edd7776d9487c74581bdf03b9f4b58adf7906f
Jerome Jaglale <jerome_jaglale@sfu.ca>
2019-04-15 12:27:44 -0700

    Add "/service/" prefix to API routes

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -65,7 +65,8 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapApiRoutes()
     {
-        Route::middleware('api')
+        Route::prefix('service')
+             ->middleware('api')
              ->namespace($this->namespace)
              ->group(base_path('routes/api.php'));
     }
